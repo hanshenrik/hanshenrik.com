70512b9a9e5696ddbbebf40c7e71cff478d0fec7
hanshenrik <hh.gronsleth@gmail.com>
2019-07-04 11:46:03 +0200

    Upgrade some stuff, do neccessary rewrite of React Accessible Accordion

--- src/components/GigList/GigList.js
+++ src/components/GigList/GigList.js
@@ -4,54 +4,52 @@ import dayjs from "dayjs";
 import {
   Accordion,
   AccordionItem,
-  AccordionItemTitle,
-  AccordionItemBody
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel
 } from "react-accessible-accordion";
 
 import gigs from "../../data/gigs.json";
 import { Gig, Table } from "../";
 import styles from "./GigList.css";
 
-class GigList extends React.Component {
-  constructor() {
-    super();
-    const sortedGigs = orderBy(gigs, ["date", "artist"], ["desc", "asc"]);
-    const groupedGigs = groupBy(sortedGigs, gig =>
-      dayjs(gig.date).format("YYYY")
-    );
-    this.state = {
-      gigsByYear: groupedGigs
-    };
-  }
+const sortedGigs = orderBy(gigs, ["date", "artist"], ["desc", "asc"]);
+const gigsByYear = groupBy(sortedGigs, gig => dayjs(gig.date).format("YYYY"));
+const preExpandedYears = Object.keys(gigsByYear).filter(
+  year => year >= dayjs().format("YYYY")
+);
 
-  render() {
-    const { gigsByYear } = this.state;
-    return (
-      <Accordion accordion={false}>
-        {Object.entries(gigsByYear)
-          .reverse()
-          .map(([year, gigsInYear]) => (
-            <AccordionItem key={year} expanded={year >= dayjs().format("YYYY")}>
-              <AccordionItemTitle>
-                <h2>
-                  <div className="accordion__arrow" />
-                  {year}
-                </h2>
-              </AccordionItemTitle>
-              <AccordionItemBody>
-                <Table className={styles.GigList}>
-                  <tbody>
-                    {gigsInYear.map((gig, index) => (
-                      <Gig gig={gig} key={index} />
-                    ))}
-                  </tbody>
-                </Table>
-              </AccordionItemBody>
-            </AccordionItem>
-          ))}
-      </Accordion>
-    );
-  }
-}
+const GigList = () => (
+  <Accordion
+    allowMultipleExpanded
+    allowZeroExpanded
+    preExpanded={preExpandedYears}
+    id="gigs"
+  >
+    {Object.entries(gigsByYear)
+      .reverse()
+      .map(([year, gigsInYear]) => (
+        <AccordionItem key={year} uuid={year}>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              <h2>
+                <div className="accordion__arrow" />
+                {year}
+              </h2>
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <Table className={styles.GigList}>
+              <tbody>
+                {gigsInYear.map((gig, index) => (
+                  <Gig gig={gig} key={index} />
+                ))}
+              </tbody>
+            </Table>
+          </AccordionItemPanel>
+        </AccordionItem>
+      ))}
+  </Accordion>
+);
 
 export default GigList;
