a25a635ca8fa3f2b9740e8ecb99ba33b24e67143
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2020-10-09 23:58:52 +0200

    Move some stuff around

--- src/components/Recording/Recording.js
+++ src/components/Recording/Recording.js
@@ -16,34 +16,41 @@ const Recording = ({ recording, ...props }) => {
   return (
     <ListItem {...props}>
       <div className={styles.Recording}>
-        <div>
+        <div className={styles.CoverAndLinks}>
           {recording.image && (
-            <img className="rounded" src={recording.image} alt={recording.name} width="200px" />
+            <img
+              className={`rounded ${styles.Cover}`}
+              src={recording.image}
+              alt={recording.name}
+              width="200px"
+            />
           )}
+          <div className={styles.Links}>
+            {recording.spotifyLink && (
+              <Link target="_blank" href={recording.spotifyLink}>
+                <FontAwesomeIcon icon={faSpotify} />
+              </Link>
+            )}
+            {recording.appleLink && (
+              <Link target="_blank" href={recording.appleLink}>
+                <FontAwesomeIcon icon={faApple} />
+              </Link>
+            )}
+            {recording.soundcloudLink && (
+              <Link target="_blank" href={recording.soundcloudLink}>
+                <FontAwesomeIcon icon={faSoundcloud} />
+              </Link>
+            )}
+            {recording.youtubeLink && (
+              <Link target="_blank" href={recording.youtubeLink}>
+                <FontAwesomeIcon icon={faYoutube} />
+              </Link>
+            )}
+          </div>
         </div>
         <div>
-          {recording.spotifyLink && (
-            <Link target="_blank" href={recording.spotifyLink}>
-              <FontAwesomeIcon icon={faSpotify} />
-            </Link>
-          )}
-          {recording.appleLink && (
-            <Link target="_blank" href={recording.appleLink}>
-              <FontAwesomeIcon icon={faApple} />
-            </Link>
-          )}
-          {recording.soundcloudLink && (
-            <Link target="_blank" href={recording.soundcloudLink}>
-              <FontAwesomeIcon icon={faSoundcloud} />
-            </Link>
-          )}
-          {recording.youtubeLink && (
-            <Link target="_blank" href={recording.youtubeLink}>
-              <FontAwesomeIcon icon={faYoutube} />
-            </Link>
-          )}
-          {recording.artist && `${recording.artist}${separator}`}
-          {recording.name && `${recording.name}${separator}`}
+          {recording.name && <span className={`${styles.Name} ${recording.image && styles.MoveLeft}`}>{recording.name && `${recording.name}${separator}`}</span>}
+          <span className={styles.Artist}>{recording.artist && `${recording.artist}${separator}`}</span>
           <ListItem.Detail>{recording.description}</ListItem.Detail>
         </div>
       </div>
