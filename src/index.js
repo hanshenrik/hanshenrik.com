31bbf7c4775c47b1251c8815edd4a96992b2537b
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2021-10-26 22:10:57 +0200

    Add polyfill to make smooth scroll work on mobile

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "typeface-muli";
+import smoothscroll from "smoothscroll-polyfill";
 
 import "./assets/react-accessible-accordion.css";
 import "./index.css";
 import App from "./App";
+
+smoothscroll.polyfill();
 
 ReactDOM.render(<App />, document.getElementById("root"));
