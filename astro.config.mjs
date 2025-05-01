06732a1b6fb767b4309fc3dd5429be5262599ac2
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2025-05-01 12:39:34 +0200

    fix broken tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [
-    tailwind(),
     icon({
       include: {
         arcticons: ["*"],
