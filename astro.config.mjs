121c3fb5275072bee7cbbe3f75340203c0a1a862
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2025-05-02 13:02:07 +0200

    add a blog, sitemap, mdx and more stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://hanshenrik.com",
   vite: {
     plugins: [tailwindcss()],
   },
@@ -14,5 +17,7 @@ export default defineConfig({
         arcticons: ["*"],
       },
     }),
+    mdx(),
+    sitemap(),
   ],
 });
---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/drypp" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    updatedDate: z.date().optional(),
+    heroImage: z.string().optional(),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = {
+  drypp: blog,
+};
