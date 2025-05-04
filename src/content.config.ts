4e195edf5664f5735f0f91653a0f5a6e2eee36d5
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2025-05-04 20:42:48 +0200

    feat: a bunch of stuff, hamburger menu and collections and such

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { file, glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
+import { v4 as uuidv4 } from "uuid";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/drypp" }),
@@ -36,7 +37,58 @@ const recordings = defineCollection({
       .optional(),
   }),
 });
+
+const gigs = defineCollection({
+  loader: file("src/data/gigs.json", {
+    parser: (text, ...props) => {
+      const data = JSON.parse(text);
+      return data.map((gig: any) => ({ id: uuidv4(), ...gig }));
+    },
+  }),
+  schema: z.object({
+    id: z.string(),
+    date: z.string(),
+    toDate: z.string().optional(),
+    venue: z.string(),
+    venueUrl: z.string().optional(),
+    city: z.string(),
+    artist: z.string(),
+    artistUrl: z.string().optional(),
+    eventDescription: z.string(),
+    eventUrl: z.string().optional(),
+    facebookUrl: z.string().optional(),
+    ticketsUrl: z.string().optional(),
+    createdAt: z.string(),
+  }),
+});
+
+const software = defineCollection({
+  loader: file("src/data/softwares.json"),
+  schema: z.object({
+    id: z.string(),
+    name: z.string(),
+    description: z.string(),
+    url: z.string().optional(),
+    githubUrl: z.string().optional(),
+  }),
+});
+
+const videos = defineCollection({
+  loader: file("src/data/videos.json"),
+  schema: z.object({
+    id: z.string(),
+    artist: z.string(),
+    name: z.string(),
+    youtubeLink: z.string(),
+    description: z.string(),
+    isPlaylist: z.boolean().optional(),
+  }),
+});
+
 export const collections = {
   drypp: blog,
   recordings,
+  gigs,
+  software,
+  videos,
 };
