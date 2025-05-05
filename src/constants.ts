e8dd3209030a5bded25a8897ee030e924a42f450
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2025-05-05 23:24:20 +0200

    feat: icons, better index page

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const RECORDINGS_ICON = "arcticons:vinyl";
+export const GIGS_ICON = "arcticons:simple-calendar";
+export const VIDEO_ICON = "arcticons:emoji-videocassette";
+export const SOFTWARE_ICON = "arcticons:code-studio";
