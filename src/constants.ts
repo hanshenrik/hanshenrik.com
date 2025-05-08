ece731c72fe6433cafaeedc2f3cb7658ba6833d7
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2025-05-08 18:15:26 +0200

    replace more icons

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,13 @@
+export const HAMBURGER_MENU_ICON = "solar:hamburger-menu-line-duotone";
+export const CLOSE_ICON = "solar:close-circle-line-duotone";
+export const TIDAL_ICON = "ph:tidal-logo-duotone";
+export const SPOTIFY_ICON = "ph:spotify-logo-duotone";
+export const APPLE_MUSIC_ICON = "ph:apple-music-logo-duotone";
+export const SOUNDCLOUD_ICON = "ph:soundcloud-logo-duotone";
+export const GITHUB_ICON = "ph:github-logo-duotone";
+export const YOUTUBE_ICON = "ph:youtube-logo-duotone";
+export const WEBSITE_ICON = "ph:globe-duotone";
+
 export const RECORDINGS_ICON = "solar:radio-minimalistic-line-duotone";
 export const GIGS_ICON = "solar:calendar-line-duotone";
 export const VIDEO_ICON = "solar:tv-line-duotone";
@@ -8,3 +18,35 @@ export const PREVIOUS_ICON = "solar:skip-previous-bold-duotone";
 export const NEXT_ICON = "solar:skip-next-bold-duotone";
 export const IS_PLAYING_ICON = "solar:volume-loud-line-duotone";
 export const PIANO_PLUKK_ICON = "ph:piano-keys-duotone";
+export const PIANO_PLUKK_NOTE_ICONS = [
+  "solar:music-note-bold",
+  "solar:music-note-bold-duotone",
+  "solar:music-note-broken",
+  "solar:music-note-line-duotone",
+  "solar:music-note-linear",
+  "solar:music-note-outline",
+  "solar:music-notes-bold",
+  "solar:music-notes-bold-duotone",
+  "solar:music-notes-broken",
+  "solar:music-notes-line-duotone",
+  "solar:music-notes-linear",
+  "solar:music-notes-outline",
+  "solar:music-note-2-bold",
+  "solar:music-note-2-bold-duotone",
+  "solar:music-note-2-broken",
+  "solar:music-note-2-line-duotone",
+  "solar:music-note-2-linear",
+  "solar:music-note-2-outline",
+  "solar:music-note-3-bold",
+  "solar:music-note-3-bold-duotone",
+  "solar:music-note-3-broken",
+  "solar:music-note-3-line-duotone",
+  "solar:music-note-3-linear",
+  "solar:music-note-3-outline",
+  "solar:music-note-4-bold",
+  "solar:music-note-4-bold-duotone",
+  "solar:music-note-4-broken",
+  "solar:music-note-4-line-duotone",
+  "solar:music-note-4-linear",
+  "solar:music-note-4-outline",
+];
